9ebd32950c4a010760f4d29574e4dbaf18d8112b
mohammad <shahrukhsiddiqui9819@gmail.com>
2025-05-03 05:48:20 +1000

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+// import { Text, View } from "react-native";
+//
+// export default function Index() {
+//   return (
+//     <View className="flex-1 justify-center items-center">
+//       <Text className="text-5xl text-blue-500  font-bold">welcome.</Text>
+//     </View
+//       >
+//   );
+// }
+import './global.css'
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-5xl text-blue-500 font-bold">welcome.</Text>
+        </View>
+    );
+}
